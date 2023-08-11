3d95ad4a02130c7a74793766cdd586feffbfdb8b
dhdl2389 <dhdl2389@naver.com>
2023-08-11 17:26:54 +0900

    20230811 finProject

---
+++ javaPrj/src/day19/Program2.java
@@ -0,0 +1,18 @@
+package day19;
+
+public class Program2 {
+
+	// 기능을 제공할 코드를 작성하고 싶어서 클래스를 작성하는 경우
+	// static 사용
+	// static 사용 안해도 됨
+
+	public static void 매서드() {
+		System.out.println("hello java !!!");
+	}
+
+	public static void main(String[] args) {
+
+		Program2.매서드();
+		
+	}
+}
